4db0ad6f2d3c5dfd3afae728cd2d6ddad5ee78db
Oraichain <oraichain@ORAICHAINS-MACBOOK-PRO.local>
2025-03-11 15:20:55 +0700

    improve authorization

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, make_response, redirect, url_for
 from flask_socketio import SocketIO
+import requests
 
 app = Flask(__name__)
 # app.config["REDIS_URL"] = "redis://127.0.0.1:6379"
@@ -8,21 +9,77 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    try:
+        return redirect(url_for('render_calendar'))
+    except:
+        return render_template('index.html')
 
 @app.route('/create-account')
 def create_account_page():
     return render_template('login.html')
 
-
 @app.route('/calendar')
 def render_calendar():
-    username = request.args.get('username', 'Guest')
-    # userID = mongo_client.find_one(collection_name='users',filter={'UserName':username})['userID']
-    userID = ""
-    return render_template('main.html', username=username,userID=userID)
-    # return render_template('main.html')
+    try:
+        username = request.args.get('username', 'Guest')
+        access_token = request.cookies.get('access_token')
 
+        if not access_token:
+            # return "Unauthorized DMM", 
+            print('EO CO ACCESS TOKEN')
+            return render_template('index.html')
+    
+        print(access_token)
+        response = requests.request("GET", 
+            url="http://localhost:5050/authorization/verify-token/", 
+            headers={
+                'accept': 'application/json',
+                'Authorization': f'Bearer {access_token}',
+                "google-auth":"true",
+            }, 
+        )
+        status_code = response.json().get('status_code',401)
+        print(response.json())
+        if status_code == 200:
+             user = response.json()['user']
+             print(user)
+             if user:
+                 username = user['full_name']
+             return render_template('main.html', username=username)
+        else:
+            print('ACCESS TOKEN EXPIRED')
+            refresh_token = request.cookies.get('refresh_token')
+            if not refresh_token:
+                print('REFRESS TOKEN KO CO')
+                return render_template('login.html')
+            
+            response = requests.request("GET", 
+                url="http://localhost:5050/authorization/verify-token/", 
+                headers={
+                    'accept': 'application/json',
+                    'Content-Type': 'application/json'
+                }, 
+                json={
+                    "refresh_token": refresh_token
+                }
+            )
+            if response.status_code != 200:
+                print('KO VERIFY DC REFRESH TOKEN')
+                return render_template('login.html')
+           
+            new_access_token = response.json()['access_token']
+            new_refresh_token = response.json()['refresh_token']
+
+            response_html = make_response(render_template('main.html', username=username))
+
+            response_html.set_cookie("access_token", new_access_token, httponly=True, secure=True)
+            response_html.set_cookie("refresh_token", new_refresh_token, httponly=True, secure=True)
+
+            return response_html
+            
+    except Exception as e:
+        print(f'Can not authorize, exception : {e}')
+        return render_template('login.html')
 
 @app.route('/week_view')
 def render_week_view():
